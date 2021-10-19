80c9fcf24211060371789a73e524582662ad448d
Daniel <dniel.rx@gmail.com>
2021-10-19 18:27:25 -0500

    listos

---
+++ ciclo/Reto3/src/main/java/Reto3/Reto3/Reto3Application.java
@@ -0,0 +1,20 @@
+package Reto3.Reto3;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+@EnableConfigurationProperties
+//@EntityScan(basePackages = {"modelo"})
+@SpringBootApplication
+public class Reto3Application {
+    
+        
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Application.class, args);
+	}
+
+}
